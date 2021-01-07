a032cede5c3f58c8e72a626682b78489f17d0bbb
adjeteGuru <justinadjev@hotmail.com>
2021-01-07 00:15:15 +0000

    feat: leaner the recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe-list/recipe.model";
 
 export class RecipeService {
+    //the property and it will hold some recipes data 
+   recipeSelected = new EventEmitter<Recipe>();
+   
     recipes: Recipe[] = [
         new Recipe('Kofte Kebab', 'This is simple grill kofta sample','https://cdn.pixabay.com/photo/2020/03/21/18/04/kabab-4954818_1280.jpg'),
         new Recipe('Steak Kebab', 'This is beef meat sample', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0mO0_VqfcYTK5jGNF4rgMFIbTisXl7nfJMQ&usqp=CAU'),
