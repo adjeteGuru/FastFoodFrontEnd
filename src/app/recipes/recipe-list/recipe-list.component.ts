281a7ab2824ad2611a39f3f9caa54de14ca1163e
adjeteGuru <justinadjev@hotmail.com>
2021-01-13 15:27:21 +0000

    feat: edit existing id set

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,12 +10,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [];  
 
-  constructor(private recipeService: RecipeService) { }  
+  constructor(private recipeService: RecipeService, private router: Router,
+    private route: ActivatedRoute) { }  
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   } 
+
+  onNewRecipe(){
+    //use this router to navigate to target the path (which is 'new') I want to go to ..
+    //bear in mind we are currently in the recipe-list
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
 
 }
